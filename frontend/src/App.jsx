ad0eadef55d495fff9bf4d6fb31d2fc92651b038
MendhaForam <158573062+MendhaForam@users.noreply.github.com>
2024-09-16 00:44:39 +0530

    added feature to add hotel and e2e test for feature

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,8 +2,12 @@ import {BrowserRouter as Router,Route,Routes,Navigate} from "react-router-dom";
 import Layout from "./layouts/Layout";
 import Register from "./pages/Register";
 import SignIn from "./pages/SignIn";
+import { useAppContext } from "./contexts/AppContext.jsx";
+import AddHotel from "./pages/AddHotel";
 
 const App = () => {
+  const {isLoggedIn} = useAppContext();
+  console.log(isLoggedIn)
   return(
     <Router>
       <Routes>
@@ -11,6 +15,9 @@ const App = () => {
         <Route path="/search" element={<Layout><p>Search Page</p></Layout>} />
         <Route path ="/register" element={<Layout><Register /></Layout>} />
         <Route path ="/sign-in" element={<Layout><SignIn /></Layout>} />
+        {isLoggedIn && (<>
+        <Route path="/add-hotel" element={<Layout><AddHotel /></Layout>} />
+        </>)}
         <Route path="*" element={<Navigate to="/" />} />
       </Routes>
     </Router>
